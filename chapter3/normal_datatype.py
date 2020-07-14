dbf5809e14e2109d751edc551de93523791b9112
Leesihyung <namils147@gmail.com>
2020-07-14 18:13:27 +0900

    first commit normal_datatype.py

---
+++ chapter3/normal_datatype.py
@@ -0,0 +1,32 @@
+a = 123
+print(type(a))
+
+a = 100 ** 2  ## 제곱
+print(a)
+
+a = 0xFF
+b = 0o77
+c = 0b1111
+print(a,b,c)
+
+a = 3.14
+b = 3.14e5
+print(a,b)
+
+a = 10; b = 20
+print(a+b, a-b, a*b, a/b)
+
+a,b = 9,2
+print(a ** b, a % b, a // b)
+
+a = True
+print(type(a))
+
+a = (100 == 100)
+b = (10 > 100)
+print(a,b)
+
+a = "파이썬 만세"
+a
+print(a)
+print(type(a))
