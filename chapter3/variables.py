ee7dfbd8471b59893234625541f121c6e3aa26e9
Leesihyung <namils147@gmail.com>
2020-07-14 18:16:38 +0900

    first commit variables.py

---
+++ chapter3/variables.py
@@ -0,0 +1,11 @@
+print("bin() : " + bin(11) +' '+ bin(0o11) +' '+ bin(0x11))
+print("oct() : " + oct(11) +' '+ oct(0b11) +' '+ oct(0x11))
+print("hex() : " + hex(11) +' '+ hex(0b11) +' '+ hex(0x11))
+
+## string ##
+a = '''
+aaa
+bbb
+ccc '''
+
+print(a)
