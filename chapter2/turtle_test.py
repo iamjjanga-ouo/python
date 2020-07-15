85377b09d64cc7d4c1ef80fe761893e7c248f631
Leesihyung <namils147@gmail.com>
2020-07-15 18:21:10 +0900

    add pos(), it return X,Y locations

--- chapter2/turtle_test.py
+++ chapter2/turtle_test.py
@@ -10,6 +10,8 @@ myT = turtle.Turtle()
 myT.shape('turtle')
 
 for i in range(0,4):
+    T = myT.pos()                       ## turtle.pos()는 vector2D를 return하는데 이는 tuple을 의미
+    print("%.2f . %.2f" % (T[0],T[1]))  ## 이동시 X,Y를 출력
     myT.forward(200)
     myT.right(90)
 
